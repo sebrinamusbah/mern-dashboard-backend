c4b0b660417aa3042cdbb201ba8d5c47868b7d21
sebrinamusbah <sebrinam@gmail.com>
2025-12-10 10:47:22 +0300

    Enable SSL for Render PostgreSQL

--- config/db.js
+++ config/db.js
@@ -5,6 +5,13 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // needed for Render Postgres
+    },
+  },
   logging: false,
 });
 
